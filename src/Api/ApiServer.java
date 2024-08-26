9f558595edeea150b2102ebadffb1eaa30865079
Suchank2059 <132805306+Suchank2059@users.noreply.github.com>
2024-08-26 13:17:22 +0545

    refactor ApiServer and Routes file

--- src/Api/ApiServer.java
+++ src/Api/ApiServer.java
@@ -1,71 +1,61 @@
 package Api;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+
+import Handlers.LoginHandler;
+import Handlers.LogoutHandler;
+import Handlers.RegisterHandler;
 import Handlers.Goals.CreateGoalHandler;
 import Handlers.Goals.DeleteGoalHandler;
 import Handlers.Goals.GetGoalHandler;
+import Handlers.Goals.ListGoalHandler;
 import Handlers.Goals.UpdateGoalHandler;
-import Handlers.LoginHandler;
-import Handlers.LogoutHandler;
 import Handlers.Tasks.CreateTaskHandler;
 import Handlers.Tasks.DeleteTaskHandler;
-import Handlers.Tasks.GetTaskHandler;
 import Handlers.Tasks.UpdateTaskHandler;
-import Middleware.TokenValidationFilter;
 import Utils.Routes;
-import com.sun.net.httpserver.HttpServer;
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import Handlers.RegisterHandler;
+import Middleware.TokenValidationFilter;
 
 public class ApiServer {
+    private static final int PORT = 9000;
+
     public static void main(String[] args) {
         try {
-            // Create an HTTP server listening on port 8000
-            HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-
-            // Define a context (endpoint) /register
-            server.createContext("/register", new RegisterHandler());
-
-            // Create context for the login handler WITHOUT token validation filter
-            server.createContext("/login", new LoginHandler());
-
-            // Register the LogoutHandler for the /logout endpoint with token validation
-            var logoutContext = server.createContext("/logout", new LogoutHandler());
-            logoutContext.getFilters().add(new TokenValidationFilter());
-
-            // Adding context for goals with token validation
-            var createGoalContext = server.createContext(Routes.CREATE_GOAL, new CreateGoalHandler());  // POST /goals/create/
-            createGoalContext.getFilters().add(new TokenValidationFilter());
-
-            var getGoalContext = server.createContext(Routes.LIST_GOAL, new GetGoalHandler()); // GET /goals/list/?id={id}
-            getGoalContext.getFilters().add(new TokenValidationFilter());
-
-            var updateGoalContext = server.createContext(Routes.UPDATE_GOAL, new UpdateGoalHandler()); // PUT /goals/update/?id={id}
-            updateGoalContext.getFilters().add(new TokenValidationFilter());
-
-            var deleteGoalContext = server.createContext(Routes.DELETE_GOAL, new DeleteGoalHandler()); // DELETE /goals/delete/?id={id}
-            deleteGoalContext.getFilters().add(new TokenValidationFilter());
-
-            // Adding context for tasks with token validation
-            var createTaskContext = server.createContext(Routes.CREATE_TASK, new CreateTaskHandler());  // POST /tasks/create/
-            createTaskContext.getFilters().add(new TokenValidationFilter());
-
-            var getTaskContext = server.createContext(Routes.LIST_TASK, new GetTaskHandler());  // POST /tasks/list/?id={id}
-            createTaskContext.getFilters().add(new TokenValidationFilter());
-
-            var updateTaskContext = server.createContext(Routes.UPDATE_TASK, new UpdateTaskHandler());  // POST /tasks/update/?id={id}
-            createTaskContext.getFilters().add(new TokenValidationFilter());
-
-            var deleteTaskContext = server.createContext(Routes.DELETE_TASK, new DeleteTaskHandler());  // POST /tasks/delete/?id={id}
-            createTaskContext.getFilters().add(new TokenValidationFilter());
-
-            server.setExecutor(null); // creates a default executor
-
-            // Start the server
+            HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
+            setupContexts(server);
+            server.setExecutor(null);
             server.start();
             System.out.println("Server started on port " + server.getAddress().getPort());
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void setupContexts(HttpServer server) {
+        // Define contexts with token validation where necessary
+        server.createContext(Routes.REGISTER, new RegisterHandler());
+        server.createContext(Routes.LOGIN, new LoginHandler());
+
+        addContextWithTokenValidation(server, Routes.LOGOUT, new LogoutHandler());
+
+        addContextWithTokenValidation(server, Routes.LIST_GOAL, new ListGoalHandler());
+        addContextWithTokenValidation(server, Routes.GET_GOAL_DETAIL, new GetGoalHandler());
+        addContextWithTokenValidation(server, Routes.CREATE_GOAL, new CreateGoalHandler());
+        addContextWithTokenValidation(server, Routes.UPDATE_GOAL, new UpdateGoalHandler());
+        addContextWithTokenValidation(server, Routes.DELETE_GOAL, new DeleteGoalHandler());
+
+        addContextWithTokenValidation(server, Routes.CREATE_TASK, new CreateTaskHandler());
+        // addContextWithTokenValidation(server, Routes.LIST_TASK, new GetTaskHandler());
+        addContextWithTokenValidation(server, Routes.UPDATE_TASK, new UpdateTaskHandler());
+        addContextWithTokenValidation(server, Routes.DELETE_TASK, new DeleteTaskHandler());
+    }
+
+    private static void addContextWithTokenValidation(HttpServer server, String path, HttpHandler handler) {
+        var context = server.createContext(path, handler);
+        context.getFilters().add(new TokenValidationFilter());
     }
 }
--- src/Utils/Routes.java
+++ src/Utils/Routes.java
@@ -1,12 +1,22 @@
 package Utils;
 
 public class Routes {
-    public static String CREATE_GOAL = "/goals/create/";
-    public static String LIST_GOAL = "/goals/list/";
-    public static String UPDATE_GOAL = "/goals/update/";
-    public static String DELETE_GOAL = "/goals/delete/";
-    public static String CREATE_TASK = "/tasks/create/";
-    public static String LIST_TASK = "/tasks/list/";
-    public static String UPDATE_TASK = "/tasks/update/";
-    public static String DELETE_TASK = "/tasks/delete/";
+    // User-related routes
+    public static final String REGISTER = "/register";
+    public static final String LOGIN = "/login";
+    public static final String LOGOUT = "/logout";
+
+    // Goal-related routes
+    public static final String LIST_GOAL = "/goals/all/";
+    public static final String CREATE_GOAL = "/goals/create";
+    public static final String GET_GOAL_DETAIL = "/goals/detail/"; // Changed to get specific goal detail
+    public static final String UPDATE_GOAL = "/goals/update";
+    public static final String DELETE_GOAL = "/goals/delete";
+
+
+    // Task-related routes
+    public static final String CREATE_TASK = "/tasks/create";
+    public static final String GET_TASK_DETAIL = "/tasks/detail"; // Changed to get specific task detail
+    public static final String UPDATE_TASK = "/tasks/update";
+    public static final String DELETE_TASK = "/tasks/delete";
 }
