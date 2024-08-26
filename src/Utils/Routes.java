495c4f310613256a410c24ff7d9a227e3408a25f
Suchank2059 <132805306+Suchank2059@users.noreply.github.com>
2024-08-26 19:41:58 +0545

    add routes for dashboard api & status toggling api

--- src/Utils/Routes.java
+++ src/Utils/Routes.java
@@ -13,10 +13,15 @@ public class Routes {
     public static final String UPDATE_GOAL = "/goals/update";
     public static final String DELETE_GOAL = "/goals/delete";
 
-
     // Task-related routes
+    public static final String LIST_TASK = "/tasks/all/";
     public static final String CREATE_TASK = "/tasks/create";
     public static final String GET_TASK_DETAIL = "/tasks/detail"; // Changed to get specific task detail
     public static final String UPDATE_TASK = "/tasks/update";
     public static final String DELETE_TASK = "/tasks/delete";
+
+    public static final String DASHBOARD = "/dashboard";
+    public static final String TOGGLE_GOAL_STATUS = "/goals/toggle-status";
+    public static final String TOGGLE_TASK_STATUS = "/tasks/toggle-status";
+
 }
